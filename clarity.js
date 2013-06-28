a83531d9b1c0524780e1b2588146ddde33f8377f
digital labs <success@dpsw.info>
2013-06-28 11:58:15 -0500

    Update clarity.js

--- clarity.js
+++ clarity.js
@@ -1,43 +1,31 @@
-var clarity = function(port, extend){
-    var app = {
-  	routes: [], filters: [],
-		all: function(rx, fn){
-			this.routes.push({rx: rx, fn: fn});
-		},
-		use: function(fn){
-			this.filters.push(fn);
-		},
-		port: port
-	}
-	
-	require('http').createServer(function(req, res) {
-    	res.setHeader('access-control-allow-origin', '*');
-    	var fin = false;
-    	app.filters.forEach(function(filter){
-    			filter(req, res);
-    	})
-	    app.routes.forEach(function(rt){
-	    		if(req.url.match(rt.rx)){
-	    			rt.fn(req, res);
-	    			fin = !fin;
-	    		}
-	    })
-	    if(!fin) res.end('');
-	}).listen(app.port);
-	console.log('listening on '+app.port);
-	return app;
-}
+require('colors');
 
-var app = clarity(8642);
-app.use(function(r, s, cb){
-	    	if(r.method == 'POST'){
-	    		var postdata;
-	        	r.on('data', function(data) {
-	    			postdata += data;
-	    		})
-	    		r.on('end', function() {
-	            	cb(require('querystring').parse(postdata));
-	        	})
-	    	}
-    
-} })
+function qs(){
+
+	var _ = require('underscore');
+
+	if(!options) options = process.env;
+	_.defaults(options, {ssl: null})
+
+	var app = {}; app.stack = [], app.use = app.stack.push;
+
+	app.use(function(r, s){
+		s.me = 1;
+	})
+
+	app.use(function(r, s){
+		s.me++;
+	})
+
+	var server = require('http' + (options.ssl == 1 ? 's':'')).createServer(function (r, s) {
+		_.each(app.stack, function(f){
+			f(r, s);
+		});
+	  	s.writeHead(200, {'Content-Type': 'text/plain'});
+	  	console.log(s.me);
+	}).listen(options.port || options.ssl == 1 ? 443:80);
+
+	console.log(server.address().green);
+	if(options.debug) console.log(options);
+}
+qs();
